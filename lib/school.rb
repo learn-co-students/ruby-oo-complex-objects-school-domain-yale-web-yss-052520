04990fae4d507aba94a9a56df26203728e43dfda
Eric Ou <eric.ou@yale.edu>
2020-05-26 14:07:02 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+class School
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ? roster[grade].append(student_name) : roster[grade] = [student_name]
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort()
+        @roster.each {|k,v| @roster[k] = v.sort()}
+        @roster
+    end
+end
