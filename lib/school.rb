055d9f64bd4512b335e9bff98bac81be6a27ab1b
nicolewang2000 <nicole.wang@yale.edu>
2020-05-26 16:39:41 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School 
+    def initialize(school)
+        @school = school 
+        @roster = {}
+    end 
+    
+    attr_accessor :roster, :name, :grade
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end 
+    
+    def grade(num)
+        roster.select {|k,v| k == num}.values.flatten
+    end 
+
+    def sort
+        sorted_hash = {}
+        roster.each {|k,v| sorted_hash[k] = v.sort}
+        sorted_hash
+    end 
+    
+end 
