be937a50485cae6ef6cb1cf23749852b9ff3d004
Joao Cardoso <joao.cardoso@yale.edu>
2020-05-26 19:44:47 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+            @roster[grade] << name
+        else
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |key, value|
+            value.sort!
+        end
+        @roster.sort.to_h
+    end
+end
